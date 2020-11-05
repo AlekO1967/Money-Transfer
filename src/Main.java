82d0ff42fc92c0cf11c7ae1e73b3ea2382a4a93e
Aleksandr Osipov <eltisprim@gmail.com>
2020-11-05 23:00:34 +0300

    Initial second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int vip_cont = 2_000_000_000;
         int transfer = 500_000_000;
-        int total = vip_cont + transfer;
-        System.out.println(total);
+        int total_balans = vip_cont + transfer;
+        System.out.println(total_balans);
     }
 }
